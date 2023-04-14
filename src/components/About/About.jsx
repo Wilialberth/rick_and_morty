d5bf66105186d5f4fc4d3b58d9a3c4529b8f9ac9
Wilialberth Jiménez <7jimenez.w@gmail.com>
2023-04-14 19:57:50 -0300

    Integrador

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,6 +6,7 @@ const About = () => {
       <h1>About me</h1>
       <p>¡Hola! Mi nombre es Wilialberth Jiménez estudiante de programación web fullstack</p>
       <p>Realicé esta aplicación con la finalidad de poner en practica mis conocimientos y habilidades.</p>
+      <p>LA PÁGINA TENDRÁ MEJORAS</p>
     </div>
   );
 }
